77c64614edba0d70acff72b63d0ae7ce8c0f6943
Juan Buys <jbuys_54@yahoo.com>
2019-02-12 18:33:43 +0200

    Initail commit to add to source control

---
+++ StockManagement.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StockManagement.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
